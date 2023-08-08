bce817ee6c0083116cbbdf46cf9b673de3bffe28
jogna <jordan.gnamien@smile.ci>
2023-08-08 12:02:22 +0000

    auto-commit

--- tests/wikipedia.test.ts
+++ tests/wikipedia.test.ts
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
-
 test.describe('Wikipedia', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('https://www.wikipedia.org/');
         await page.locator('strong:has-text("Français")').click();
     })
 
-test.only('Trouve un raton laveur sur wikipedia', async ({ page }) => {
+test('Trouve un raton laveur sur wikipedia', async ({ page }) => {
     await page.locator('[placeholder="Rechercher sur Wikipédia"]').click()
     await page.keyboard.type('raton laveur')
     await page.keyboard.press('Enter')
